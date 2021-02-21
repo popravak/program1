2516fd646ced46aee182ec9a2fd249767adb7256
Sasa <sasa.popravak@gmail.com>
2021-02-21 11:50:02 +0100

    For Loop

--- src/print_function.h
+++ src/print_function.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int print_from_function () {
-    printf ("STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n");
-
+    for (i == 1; i <= 10; i++) {
+        printf ("STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n");
+    }
     return 0;
 }
