0ad10672968295aa01ef46d69d0fc575117db879
Sasa <sasa.popravak@gmail.com>
2021-02-22 18:18:05 +0100

    .

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include "/var/lib/jenkins/workspace/program1/src/print_function.h"
+#include "../src/print_function.h"
 
 int main (void) {
 
    print_from_function ();
-
-   printf ("TREBALO MI JE OKRUGLO 40 BILDOVANJA DA ZAVRSIM!\n");
 
    return 0;
 }
