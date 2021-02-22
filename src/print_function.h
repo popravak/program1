b82a92259f043cfd1897c112768273147a920128
Sasa <sasa.popravak@gmail.com>
2021-02-22 17:54:13 +0100

    .

--- src/print_function.h
+++ src/print_function.h
@@ -2,8 +2,10 @@
 
 int print_from_function () {
     for (int i = 1; i <= 10; i++) {
-        printf ("%d. STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n", i);
-        //printf ("BLAH!!!\n");
+        //printf ("%d. STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n", i);
     }
+
+    printf ("\nBRANCH: for-loop.\n");
+
     return 0;
 }
