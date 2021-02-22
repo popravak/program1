5783674efafe823600b5be59cb0e4e344a73f62f
Sasa <sasa.popravak@gmail.com>
2021-02-22 17:52:11 +0100

    .

--- src/print_function.h
+++ src/print_function.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int print_from_function () {
-    printf ("STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n");
+    //printf ("STVANO SI SMRADINA STO SI ME PUSTIO DA SE SAM PATIM :)\n");
+
+    printf ("\nBRANCH: main.\n");
 
     return 0;
 }
